5ab42f9077e0df838b70f2d1777f8b932e57650b
Matthew Frankland <admin@matthewfrankland.co.uk>
2025-02-15 21:16:20 +0000

    Removed More Packages

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,19 +1,15 @@
-import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection } from "@angular/core"
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from "@angular/core"
 import { provideRouter } from "@angular/router"
 import { routes } from "./app.routes"
 import { provideHttpClient } from "@angular/common/http"
 import { provideAnimations } from "@angular/platform-browser/animations"
-import { FormlyBootstrapModule } from "@ngx-formly/bootstrap"
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection ( ),
     provideRouter ( routes ),
     provideHttpClient ( ),
-    provideAnimations ( ),
-    importProvidersFrom (
-      FormlyBootstrapModule
-    )
+    provideAnimations ( )
   ]
 }
 
